8ef3a5a3bd07a5b488ba1388d14b5e115d849c55
Abul Hussain <abul.hussain@jadu.co.uk>
2018-04-13 14:27:55 +0100

    User adapter interface added

---
+++ src/User/Authentication/AuthenticationAdapterInterface.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Jadu\ContinuumCommon\User\Authentication;
+
+use Symfony\Component\HttpFoundation\Request;
+
+interface AuthenticationAdapterInterface
+{
+    public function authenticate(Request $request);
+
+    public function canRegister();
+
+    public function canUpdate();
+
+    public function canUpdatePassword();
+
+    public function canRemindPassword();
+
+    public function getSignInURL();
+
+    public function getSignOutURL();
+
+    public function getRegisterURL();
+
+    public function getAccountURL();
+
+    public function getPasswordReminderURL();
+
+    public function getUpdateURL();
+
+    public function getUpdatePasswordURL();
+
+    public function initSession();
+
+    public function performLogout();
+
+    public function isLoginAction(Request $request);
+
+    public function getFailedLoginResponseUrl();
+
+    public function getMachineKey();
+}
